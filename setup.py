90c076e6ded06e11f7dc6004e1688c0b678c6ce5
sunit1409 <sunit140995@gmail.com>
2025-04-13 05:43:17 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name = "src",
+    version="0.0.1",
+    author = "sunit adhikary",
+    author_email= "sunit140995@gmail.com",
+    packages = find_packages()
+)
